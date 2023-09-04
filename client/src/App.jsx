91e390fd17378ab3b7e0e61a60c5d43a327e306e
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2023-09-04 20:26:43 +0530

    feat: Install necessary tools and libraries

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="flex flex-wrap justify-center items-center text-center bg-black h-screen gap-5 ">
+      <h1 className="font-bold text-9xl text-red-600">StudyFlix</h1>
+
+      <h1 className="font-bold text-7xl text-white">coming soon...</h1>
+    </div>
+  );
+}
