e90fcf9655df5c2ed29d3feff1576b1dc4f03915
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2023-09-16 15:25:43 +0530

    Changes some file :)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,35 @@
-/** @format */
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-export default function App() {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="flex flex-wrap justify-center items-center text-center bg-black h-screen gap-5 ">
-      <h1 className="font-bold text-9xl text-red-600">StudyFlix</h1>
-
-      <h1 className="font-bold text-7xl text-white">coming soon...</h1>
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
+
+export default App
