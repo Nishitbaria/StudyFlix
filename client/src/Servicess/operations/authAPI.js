9d6911454f4286b4adc4176ace02e8b5b2f1ca6d
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2023-09-21 15:12:12 +0530

    fix : Vite Path

--- client/src/Servicess/operations/authAPI.js
+++ client/src/Servicess/operations/authAPI.js
@@ -6,6 +6,7 @@ import { setUser } from "../../slices/profileSlice"
 import { apiConnector } from "../apiConnector"
 import { endpoints } from "../apis"
 
+
 const {
   SENDOTP_API,
   SIGNUP_API,
@@ -119,8 +120,14 @@ export function login(email, password, navigate) {
 
 export function getPasswordResetToken(email, setEmailSent) {
   return async (dispatch) => {
+    
+    console.log("RESETPASSTOKEN API............", RESETPASSTOKEN_API);
+    console.log(import.meta.env.VITE_REACT_APP_BASE_URL)
     const toastId = toast.loading("Loading...")
+    
     dispatch(setLoading(true))
+
+
     try {
       const response = await apiConnector("POST", RESETPASSTOKEN_API, {
         email,
