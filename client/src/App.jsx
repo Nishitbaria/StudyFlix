e3aaaffd6aab6a0791fcdcc04883da9481277d88
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2023-09-21 10:26:16 +0530

    fix : path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,8 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Navbar from "./components/common/Navbar";
 import OpenRoute from "./components/core/Auth/OpenRoute";
-
+import Navbar from "./components/common/Navbar";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import ForgotPassword from "./pages/ForgotPassword";
