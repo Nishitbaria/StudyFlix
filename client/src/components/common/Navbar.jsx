3820d1631ce12b4a2c6c87ec4c68dad26af3177e
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2023-10-01 14:51:36 +0000

    fix:ProfileDropDown import

--- client/src/components/common/Navbar.jsx
+++ client/src/components/common/Navbar.jsx
@@ -10,7 +10,7 @@ import { NavbarLinks } from "../../data/navbar-links"
 import { apiConnector } from "../../services/apiconnector"
 import { categories } from "../../services/apis"
 import { ACCOUNT_TYPE } from "../../utils/constants"
-import ProfileDropdown from "../core/Auth/ProfileDropdown"
+import ProfileDropdown from "../core/Auth/ProfileDropDown"
 
 const subLinks = [
   {
