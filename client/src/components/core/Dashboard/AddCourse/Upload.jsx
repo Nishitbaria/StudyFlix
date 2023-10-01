50f73baf0282cb34e0a717dbf83dc8cb66a283a9
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2023-10-01 15:02:59 +0000

    fix: import issue of useDropZone

--- client/src/components/core/Dashboard/AddCourse/Upload.jsx
+++ client/src/components/core/Dashboard/AddCourse/Upload.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react"
-import { useDropzone } from "react-dropzone"
+import {useDropzone} from 'react-dropzone'
 import { FiUploadCloud } from "react-icons/fi"
 import { useSelector } from "react-redux"
 
